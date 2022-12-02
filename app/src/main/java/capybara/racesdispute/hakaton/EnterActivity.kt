e315858c05356e45766b16186a953365c9d6b19f
polichinel <bezborodovstanislav109@gmail.com>
2022-12-03 00:40:38 +0300

    Add classes

---
+++ app/src/main/java/capybara/racesdispute/hakaton/EnterActivity.kt
@@ -0,0 +1,15 @@
+package capybara.racesdispute.hakaton
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EnterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.enter)
+
+    }
+
+
+}
---
+++ app/src/main/java/capybara/racesdispute/hakaton/SubmitActivity.kt
@@ -0,0 +1,15 @@
+package capybara.racesdispute.hakaton
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SubmitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.submit)
+
+    }
+
+
+
+}
